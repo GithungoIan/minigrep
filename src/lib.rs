c741fcfdd61b6d2d04c98e45cb8720a915e286d9
GithungoIan <githungoian53@gmail.com>
2020-08-09 14:30:05 +0300

    write fail search fn

--- src/lib.rs
+++ src/lib.rs
@@ -21,11 +21,33 @@ impl Config{
     }
 }
 
- //extract logic from main
+//extract logic from main
 //return errors from the run fn
 pub fn run(config: Config) -> Result<(), Box<dyn Error>>{
     let contents = fs::read_to_string(config.filename)?;
     println!("with text\n{}", contents);
 
     Ok(())
+}
+
+
+pub fn search<'a>(query: &str, contents: &'a str ) -> Vec<&'a str>{
+    vec![]
+}
+//********Test Driven Development*************
+
+#[cfg(test)]
+mod tests{
+     use super::*;
+
+    #[test]
+    fn one_result(){
+        let query = "duct";
+        let contents = "\
+ Rust:\
+ safe, fast, productive.\
+ Pick three";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
 }

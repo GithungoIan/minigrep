eb89b7c383d2f66cd295a468a15914e03a414de0
GithungoIan <githungoian53@gmail.com>
2020-08-09 13:50:18 +0300

    read contents from file

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,18 @@
-
-// Accepting command line arguments
 use std::env;
+use std::fs;
+
+// Steps in building a minigrep command line application
+// Accepting command line arguments
+// Reading a file
 fn main() {
 
     let args:Vec<String> = env::args().collect();
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for {}", query);
+    println!("In file {}", filename);
+
+    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    println!("with text\n{}", contents);
 }

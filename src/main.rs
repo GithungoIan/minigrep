4e7c545b2e0f7b288313482d347c23f105511722
GithungoIan <githungoian53@gmail.com>
2020-08-09 13:53:39 +0300

    extracting the argument_parser

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,19 @@ use std::fs;
 fn main() {
 
     let args:Vec<String> = env::args().collect();
-    let query = &args[1];
-    let filename = &args[2];
+
+    let (query, filename) = argument_parser(&args);
 
     println!("Searching for {}", query);
     println!("In file {}", filename);
 
     let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
     println!("with text\n{}", contents);
+}
+
+fn argument_parser(args: &[String]) -> (&str, &str){
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
 }
